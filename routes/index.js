ee339a6fd7456de1e62793aa7c98a1b4ae5492f8
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2020-01-18 12:07:52 +0100

    Changes made to app.js, routes and todos.js

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import TodoController from '../todosControllers/todos';
+
+const router = express.Router();
+
+
+router.get('/api/v1/todos', TodoController.getAllTodos);
+router.get('/api/v1/todos/:id', TodoController.getTodo);
+router.post('/api/v1/todos', TodoController.createTodo);
+router.put('/api/v1/todos/:id', TodoController.updateTodo);
+router.delete('/api/v1/todos/:id', TodoController.deleteTodo);
+
+
+export default router;
